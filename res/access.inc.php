42450f43b2f252e3ff23eeffa4136832b86449a9
MineStroyMC <64633686+MineStroyMC@users.noreply.github.com>
2020-05-16 08:16:46 +0300

    Create access.inc.php

---
+++ res/access.inc.php
@@ -0,0 +1,23 @@
+<?php
+
+// Users data
+$imSettings['access']['webregistrations_gid'] = 'izo44q5m';
+$imSettings['access']['users'] = array(
+	'fyfyfh@gmail.com' => array(
+		'groups' => array('5y9ffcpl'),
+		'id' => '91394qsv',
+		'firstname' => 'Admin',
+		'lastname' => '',
+		'password' => 'KR12kg',
+		'email' => 'fyfyfh@gmail.com',
+		'page' => false
+	)
+);
+
+// Admins list
+$imSettings['access']['admins'] = array('91394qsv');
+
+// Page/Users permissions
+$imSettings['access']['pages'] = array();
+
+// End of file access.inc.php
